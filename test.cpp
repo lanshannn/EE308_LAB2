7efcadc2733f84cedb3efc2c11668ba8eb0d0d69
阑珊 <90843963+lanshannn@users.noreply.github.com>
2021-09-23 11:51:46 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -7,33 +7,33 @@ int main(){
         case 0:  
             break;  
         case 1:  
-	          break;  
-	      case 2:  
-		        break;  
-		    default:  
-	          break;  
-	  }  
-	  switch(i){  
-	      case 0:  
-		        break;  
-		    case 1:  
-		        break;  
-		    default:  
-			      break;  
-	  }  
-	  if(i<0){  
-		    if(i<-1){}  
-	       else{}  
-	  }  
-	  else if(i>0){  
-		    if (i>2){}  
-		    else if (i==2) {}  
-		    else if (i>1) {}  
-		  else {}  
-	  }  
-	  else{  
-		    if(j!=0){}  
-		    else{}  
-	  }  
-	  return 0;  
+	    break;  
+	case 2:  
+	    break;  
+	default:  
+	    break;  
+    }  
+    switch(i){  
+	case 0:  
+	    break;  
+	case 1:  
+	    break;  
+	default:  
+	    break;  
+    }  
+    if(i<0){  
+	if(i<-1){}  
+	else{}  
+    }  
+    else if(i>0){  
+	if (i>2){}  
+	else if (i==2) {}  
+	else if (i>1) {}  
+	else {}  
+    }  
+    else{  
+	if(j!=0){}  
+	else{}  
+    }  
+    return 0;  
 }  
